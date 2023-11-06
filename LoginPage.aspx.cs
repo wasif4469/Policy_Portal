1ba5a8b75e8ea3fa98bae015ff395ee607fa7767
MWasif <wasifnaziree@gmail.com>
2023-11-06 14:02:39 +0500

    DONE 6/11/2023 2:02 PM

--- LoginPage.aspx.cs
+++ LoginPage.aspx.cs
@@ -42,17 +42,17 @@ namespace Policy_Portal
             if (is_valid)
             {
                 // Check if the AD username matches a username in the PortalUsers table
-                if (IsAuthorizedUser(ADuser, out string role, out string employeeID, out string fullname, out string Department, out string userName, out string TeamName, out string Email))
+                if (IsAuthorizedUser(ADuser, out string fullname, out string userName))
                 {
                     // Store user data in session variables
                     Session["IsLoggedIn"] = true;
-                    Session["UserRole"] = role;
+                    //Session["UserRole"] = role;
                     Session["UserName"] = userName;
-                    Session["EmployeeID"] = employeeID;
-                    Session["Department"] = Department;
+                   // Session["EmployeeID"] = employeeID;
+                   //Session["Department"] = Department;
                     Session["FullName"] = fullname;
-                    Session["TeamName"] = TeamName;
-                    Session["Email"] = Email;
+                   // Session["TeamName"] = TeamName;
+                   // Session["Email"] = Email;
 
                     Response.Redirect("PolicyForm.aspx");
                 }
@@ -70,13 +70,14 @@ namespace Policy_Portal
             }
         }
 
-            private bool IsAuthorizedUser(string username, out string role, out string employeeID, out string fullname, out string Department, out string userName, out string TeamName, out string Email)
+            private bool IsAuthorizedUser(string username, out string fullname, out string userName)
             {
                 string connectionString = ConfigurationManager.ConnectionStrings["ConnectionStringDB"].ConnectionString;
-                role = employeeID = fullname = Department = userName = TeamName = Email = string.Empty;
+               // role = employeeID = fullname = Department = userName = TeamName = Email = string.Empty;
+               userName = fullname = string.Empty;
                 using (SqlConnection connection = new SqlConnection(connectionString))
                 {
-                    string query = "SELECT * FROM PortalUsers WHERE UserName = @UserName";
+                    string query = "SELECT * FROM IT_USER WHERE Username = @UserName";
 
                     using (SqlCommand command = new SqlCommand(query, connection))
                     {
@@ -87,13 +88,13 @@ namespace Policy_Portal
 
                         if (reader.Read())
                         {
-                            Email = reader["Email"].ToString();
-                            role = reader["Designation"].ToString();
-                            employeeID = reader["EmployeeID"].ToString();
-                            fullname = reader["FullName"].ToString();
-                            Department = reader["Department"].ToString();
-                            userName = reader["UserName"].ToString();
-                            TeamName = reader["TeamName"].ToString();
+                           // Email = reader["Email"].ToString();
+                           // role = reader["Designation"].ToString();
+                          //  employeeID = reader["EmployeeID"].ToString();
+                            fullname = reader["Name"].ToString();
+                           // Department = reader["Department"].ToString();
+                            userName = reader["Username"].ToString();
+                           // TeamName = reader["TeamName"].ToString();
                             return true;
                         }
                     }
